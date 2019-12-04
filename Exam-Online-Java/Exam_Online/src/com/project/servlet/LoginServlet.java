d888802b1307851783b41170789f9d221cc02b9e
Watermelon <1050364782@qq.com>
2019-12-04 21:28:34 +0800

    Exam-Online v1.0 des:project is start

--- Exam-Online-Java/Exam_Online/src/com/project/servlet/LoginServlet.java
+++ Exam-Online-Java/Exam_Online/src/com/project/servlet/LoginServlet.java
@@ -8,16 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.project.DAO.ProjectDAO;
+
 public class LoginServlet extends HttpServlet {
 	public void service(HttpServletRequest request,HttpServletResponse response) throws IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
+		ProjectDAO projectDAO = new ProjectDAO();
 		String username = (String)request.getParameter("username");
 		String password = (String)request.getParameter("password");
 		System.out.println(username);
 		System.out.println(password);
 		PrintWriter writer = response.getWriter();
-		if(username.equals("lalala")&&password.equals("12345")) {
+		if(projectDAO.isUserExists(username)) {
 			writer.println("µÇÂ½³É¹¦£¡");
 		}else {
 			writer.println("µÇÂ½Ê§°Ü£¬ÇëÖØÐÂµÇÂ¼£¡");
